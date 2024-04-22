09ededb6baba568378d19cfd65e46bd7bd13e25b
Patricia Byrro <pbbyrro@gmail.com>
2024-04-22 11:25:37 -0300

    Criação de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from './pages/home/Home';
 import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
+import DeletarCategoria from './components/categorias/deletarCategoria/DeletarCategoria';
 
 function App() {
   return (
@@ -14,6 +17,10 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
             </Routes>
           </div>
           <Footer />
--- src/components/categorias/listaCategorias/ListaCategorias.tsx
+++ src/components/categorias/listaCategorias/ListaCategorias.tsx
@@ -1,19 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react';
-import CardCategorias from '../cardCategorias/CardCategorias';
+import { useNavigate } from 'react-router-dom';
+import Categoria from '../../../models/Categoria';
+import { buscar } from '../../../services/Service';
+import CardCategoria from '../cardCategorias/CardCategorias';
 
 function ListaCategorias() {
-  
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+
+  let navigate = useNavigate();
+
   return (
     <>
-    
-      <div className="flex justify-center w-full my-4">
-        <div className="container flex flex-col">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              <>
-                <CardCategorias  />
-              </>
-          </div>
-        </div>
+      {categorias.length === 0}
+      <div className='container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {categorias.map((categoria) => (
+          <CardCategoria key={categoria.id} categoria={categoria} />
+        ))}
       </div>
     </>
   );
